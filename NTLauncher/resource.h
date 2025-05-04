05959027c8447b804a0a58b21aa19e63bdd64cf1
acomma <acomma@yeah.net>
2025-05-04 11:31:27 +0800

    创建通知图标弹出菜单资源

--- NTLauncher/resource.h
+++ NTLauncher/resource.h
@@ -2,14 +2,22 @@
 // Microsoft Visual C++ generated include file.
 // Used by NTLauncher.rc
 //
-#define IDI_NTLAUNCHER                  101
+#define IDI_NTLAUNCHER                           101
+#define IDR_NOTIFYICON_MENU                      102
+#define ID_NOTIFYICON_MENU_ABOUT                 40001
+#define ID_NOTIFYICON_MENU_SETTING               40002
+#define ID_NOTIFYICON_MENU_ADDON_MANAGE          40003
+#define ID_NOTIFYICON_MENU_SEARCH_PICTURE        40004
+#define ID_NOTIFYICON_MENU_BATCH_OCR             40005
+#define ID_NOTIFYICON_MENU_SCAN_QRCODE           40006
+#define ID_NOTIFYICON_MENU_EXIT                  40007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40008
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
