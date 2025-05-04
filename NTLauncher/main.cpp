93667c820ea8cc16d10250499778dbab9506e0d7
acomma <acomma@yeah.net>
2025-05-04 11:33:12 +0800

    显示通知图标及弹出菜单

--- NTLauncher/main.cpp
+++ NTLauncher/main.cpp
@@ -1,6 +1,14 @@
 #include <windows.h>
 #include <tchar.h>
+#include <strsafe.h>
 #include "resource.h"
+
+// 通知图标消息
+#define WM_NOTIFYICON (WM_USER + 1)
+
+HINSTANCE hInst;
+// 通知图标数据
+NOTIFYICONDATA nid;
 
 // 窗口过程
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -31,6 +39,9 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 		return 1;
 	}
 
+	// 保存实例句柄
+	hInst = hInstance;
+
 	// 创建窗口
 	HWND hWnd = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW, _T("NTLauncher"), _T("NTLauncher"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 100, NULL, NULL, hInstance, NULL);
 
@@ -43,6 +54,20 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	// 显示窗口
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
+
+	// 显示通知图标
+	nid.cbSize = sizeof(NOTIFYICONDATA);
+	nid.hWnd = hWnd;
+	nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_GUID;
+	nid.uCallbackMessage = WM_NOTIFYICON;
+	nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_NTLAUNCHER));
+	nid.guidItem = { 0x30549556, 0x75c2, 0x452a, { 0xaf, 0x3c, 0x12, 0x32, 0xb, 0x53, 0x4e, 0x9 } }; // {30549556-75C2-452A-AF3C-12320B534E09}
+	StringCchCopy(nid.szTip, ARRAYSIZE(nid.szTip), _T("NTLauncher"));
+	if (Shell_NotifyIcon(NIM_ADD, &nid) != TRUE)
+	{
+		MessageBox(NULL, _T("调用 Shell_NotifyIcon 失败！"), _T("NTLauncher"), NULL);
+		return 1;
+	}
 
 	// 消息循环
 	MSG msg;
@@ -64,6 +89,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
 		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_NOTIFYICON:
+	{
+		// 显示通知图标弹出菜单
+		if (lParam == WM_RBUTTONUP)
+		{
+			POINT pt;
+			GetCursorPos(&pt);
+
+			HMENU hMenu = LoadMenu(hInst, MAKEINTRESOURCE(IDR_NOTIFYICON_MENU));
+			HMENU hPopup = GetSubMenu(hMenu, 0);
+
+			if (hPopup)
+			{
+				SetForegroundWindow(hWnd);
+				TrackPopupMenu(hPopup, TPM_LEFTALIGN | TPM_RIGHTBUTTON, pt.x, pt.y, 0, hWnd, NULL);
+			}
+
+			DestroyMenu(hMenu);
+		}
 	}
 	break;
 	case WM_DESTROY:
